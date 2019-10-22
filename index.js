d9250386fe0f6c14ccde0d935a6d36390142b880
huang.zw <huangzhiwu@MacBook-Pro-75.local>
2019-10-22 19:26:59 +0800

    style: modify the content of page.

--- index.js
+++ index.js
@@ -6,13 +6,74 @@ var server = app.listen(4000, () => {
     console.log('listening for requests on port 4000,');
 });
 
+
 var io = socket(server);
 io.on('connection', (socket) => {
-
+    
     console.log('made socket connection', socket.id);
 
-    socket.on('send', (data) => {
-        socket.broadcast.emit('receive', data);
-    });
+    // chatroom page
+    handle_chatroom_page(socket);
+
+    // editor page
+    handle_whiteboard_page(socket);
+
+    // whiteboard page
+    handle_editor_page(socket);
 
 });
+
+// chatroom page
+var numUsers = 0;
+var handle_chatroom_page = (socket) => {
+  var addedUser = false;
+
+  socket.on('new message', (data) => {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+};
+
+// whiteboard page
+var handle_whiteboard_page = (socket) => {}
+
+// editor page
+var handle_editor_page = (socket) => {}
